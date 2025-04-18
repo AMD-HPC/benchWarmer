3af7fb7b33967a1140663e28b3f3fbf2771c8fa1
Kyle Hoffmeyer <97640268+unagiboi@users.noreply.github.com>
2025-04-18 16:37:51 -0500

    rsqrt_fix + mem_access_fix (#16)
    * rsqrt_fix + mem_access_fix
    
    * use original throughput kernel for rsqrt
    
    * update packed kernel to be consistent with other kernels
    
    ---------
    
    Co-authored-by: Andrew Chisolm <Andrew.Chisolm@amd.com>

--- benchWarmer.cu
+++ benchWarmer.cu
@@ -169,10 +169,9 @@ __global__ void throughput_kernel(T *buf, uint32_t nSize)
 		#pragma unroll
 		for(int j=0; j<n; j++)
 		{
-			x = func(a[offset], x, y);
+			a[offset] = func(a[offset], x, y);
 		}
 	}
-  a[0] = x;
 }
 
 // This kernel is similar to throughput_kernel, but it employs a workaround to prevent
@@ -223,10 +222,9 @@ __global__ void packed_throughput_kernel(float2 *buf, uint32_t nSize)
 		#pragma unroll
 		for(int j=0; j<n; j++)
 		{
-			x = {func(a[offset].x, x.x, y.x), func(a[offset].y, x.y, y.y)};
+			a[offset] = {func(a[offset].x, x.x, y.x), func(a[offset].y, x.y, y.y)};
 		}
 	}
-	a[0] = x;
 }
 
 
@@ -266,6 +264,8 @@ static void bench_func(void) {
   uint64_t nThreads = (uint64_t)numWorkgroups * (uint64_t)workgroupSize;
   int nSize = DEFAULT_DATASET_SIZE/sizeof(T);  // total number of ints/floats
   uint64_t totalFlops = (uint64_t)nSize  * (uint64_t)nOps;
+  // Each element in memory gets one read and one write
+  uint64_t totalBytes = (uint64_t)nSize * (uint64_t)sizeof(T) * 2;
 
   // Map to get common names of datatypes
   std::unordered_map<std::string, std::string> typeNameLookup = {
@@ -293,7 +293,6 @@ static void bench_func(void) {
 	if(op == "MulAdd") {
 		totalFlops *= 2;
 	}
-  uint64_t totalBytes = (uint64_t)nSize * (uint64_t)sizeof(T);
 
   T *memBlock;
   assert((gpu(Malloc((void**)&memBlock, DEFAULT_DATASET_SIZE)))==gpu(Success));
@@ -312,7 +311,7 @@ static void bench_func(void) {
 
   // WARMUP KERNEL
   // packed_throughput_kernel: FP32 Add, Mul, MulAdd
-  // throughput_kernel_unrolled: Rsqrt, All Integer Add, Mul
+  // throughput_kernel_unrolled: All Integer Add, Mul
   // throughput_kernel: All other tests
   if (datatype == "fp32" && (op == "MulAdd")) {
     // FP32 MulAdd
@@ -323,8 +322,8 @@ static void bench_func(void) {
   } else if (datatype == "fp32" && (op == "Mul")) {
     // FP32 Mul
     packed_throughput_kernel<nOps,Mul<float>><<<dim3(numWorkgroups), dim3(workgroupSize)>>>((float2 *)memBlock, nSize/2);
-  } else if ((datatype.find("int") != std::string::npos && (op == "Add" || op == "Mul")) || op == "Rsqrt") {
-    // Rsqrt, Integer Add, Mul
+  } else if (datatype.find("int") != std::string::npos && (op == "Add" || op == "Mul")) {
+    // Integer Add, Mul
     throughput_kernel_unrolled<T,nOps,Func><<<dim3(numWorkgroups), dim3(workgroupSize)>>>((T *)memBlock, nSize);
   } else {
     // Every other test
@@ -348,7 +347,7 @@ static void bench_func(void) {
     gpu(DeviceSynchronize());
 
 		// packed_throughput_kernel: FP32 Add, Mul, MulAdd
-		// throughput_kernel_unrolled: Rsqrt, All Integer Add, Mul
+		// throughput_kernel_unrolled: All Integer Add, Mul
 		// throughput_kernel: All other tests
     if (datatype == "fp32" && (op == "MulAdd")) {
       // FP32 MulAdd
@@ -365,8 +364,8 @@ static void bench_func(void) {
 			initTimeEvents(start, stop);
       packed_throughput_kernel<nOps,Mul<float>><<<dim3(numWorkgroups), dim3(workgroupSize)>>>((float2 *)memBlock, nSize/2);
 			stopTimeEvents(eventMs, start, stop);
-    } else if ((datatype.find("int") != std::string::npos && (op == "Add" || op == "Mul")) || op == "Rsqrt") {
-      // Rsqrt, Integer Add, Mul
+    } else if (datatype.find("int") != std::string::npos && (op == "Add" || op == "Mul")) {
+      // Integer Add, Mul
 			initTimeEvents(start, stop);
       throughput_kernel_unrolled<T,nOps,Func><<<dim3(numWorkgroups), dim3(workgroupSize)>>>((T *)memBlock, nSize);
 			stopTimeEvents(eventMs, start, stop);
