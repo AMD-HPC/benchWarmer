4cc82f20ae6ac25f53cfe3342de649a96b83332e
unagiboi <khoffmey@amd.com>
2025-04-23 11:05:47 -0500

    mi250x up to 128

--- run_pubbench_mi250x.sh
+++ run_pubbench_mi250x.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ##SBATCH -w x1000c3s5b1n0
 ##SBATCH -w x1000c2s5b1n0
+#SBATCH -w x1000c3s1b1n0
 #SBATCH --gpus-per-node=1
 #SBATCH --time=36:00:00
 #SBATCH --output=./slurm_output/mi250x.out
@@ -13,6 +14,7 @@
 # pwd
 # ls
 # module load rocm/6.1.1
+module load rocm
 
 nOps=()
 # nOps=2
@@ -39,33 +41,38 @@ for nOp in "${nOps[@]}"; do
     echo "Running PubBench with $nOp ops"
     # make clean
     EXP=10
-    make -B amd nOps=$nOp GPU=$GPU ROCSTAR_ROOT=$ROCSTAR_ROOT EXP=$EXP
-    # ./benchWarmer-amd_${GPU}_${nOp}_${EXP} -u "$GPU" --muladd --fp32
-    # AVG_TIME=$(cat meanDuration.txt)
-    # EXP=$(echo "(20000 / $AVG_TIME + 0.5)/1" | bc)
-    # echo "New exp: $EXP"
+    make amd nOps=$nOp GPU=$GPU ROCSTAR_ROOT=$ROCSTAR_ROOT EXP=$EXP
+    ./benchWarmer-amd_${GPU}_${nOp}_${EXP} -u "$GPU" --muladd --fp32
+    AVG_TIME=$(cat meanDuration.txt)
+    BASE_EXP=$(echo "$AVG_TIME" | awk '{ x = int((20000 / $1) + 0.5); if (x > 10) print x; else print 10 }')
+    echo "New exp: $BASE_EXP"
+    make amd nOps=$nOp GPU=$GPU ROCSTAR_ROOT=$ROCSTAR_ROOT EXP=$BASE_EXP
 
     for opType in "${opTypes[@]}"; do
         for dataType in "${dataTypes[@]}"; do
-            EXP=10
+            export ROCSTAR_OUTPUT_FILE="./rocstar_metrics/$GPU/hw_metrics_${nOp}_${opType}_${dataType}.csv"
+            EXP=$BASE_EXP
+            if [[ "$dataType" == "int64" || "$opType" == "div" || "$opType" == "rsqrt" ]]; then
             # echo "nOps: ${nOp}, opType: ${opType}, dataType: ${dataType}"
-            ./benchWarmer-amd_${GPU}_${nOp}_${EXP} -u "$GPU" --"$opType" --"$dataType"
-
-            AVG_TIME=$(cat meanDuration.txt)
-            EXP=$(echo "(20000 / $AVG_TIME + 0.5)/1" | bc)
-            echo "New exp: $EXP"
+                ./benchWarmer-amd_${GPU}_${nOp}_10 -u "$GPU" --"$opType" --"$dataType"
+                AVG_TIME=$(cat meanDuration.txt)
+                EXP=$(echo "$AVG_TIME" | awk '{ x = int((20000 / $1) + 0.5); if (x > 10) print x; else print 10 }')
+                make amd nOps=$nOp GPU=$GPU ROCSTAR_ROOT=$ROCSTAR_ROOT EXP=$EXP
+            fi
+            # EXP=$(echo "$AVG_TIME" | awk '{ x = int((20000 / $1) + 0.5); if (x > 10) print x; else print 10 }')
+            # echo "New exp: $EXP"
             # ((EXP=17/AVG_TIME))
             
             # if AI >
             # nOp=8
             # opType="muladd"
             # dataType="int64"
-            export ROCSTAR_OUTPUT_FILE="./rocstar_metrics/$GPU/hw_metrics_${nOp}_${opType}_${dataType}.csv"
+            # export ROCSTAR_OUTPUT_FILE="./rocstar_metrics/$GPU/hw_metrics_${nOp}_${opType}_${dataType}.csv"
             # export AGT_OUTPUT_FILE="./agt_metrics/$GPU/hw_metrics_${nOp}_${opType}_${dataType}.csv"
             # AGT_CMD="sudo $AGT_PATH -unilog=PM -unilogallgroups -i=0,1,2,3,4,5,6,7 -unilogperiod=50 -unilogoutput=${AGT_OUTPUT_FILE} &"
             # eval $AGT_CMD
             # echo "Starting AGT: $(date)"
-            make -B amd nOps=$nOp GPU=$GPU ROCSTAR_ROOT=$ROCSTAR_ROOT EXP=$EXP
+            # make -B amd nOps=$nOp GPU=$GPU ROCSTAR_ROOT=$ROCSTAR_ROOT EXP=$EXP
             ./benchWarmer-amd_${GPU}_${nOp}_${EXP} -u "$GPU" --"$opType" --"$dataType"  --rocstar --record
         done
     done
